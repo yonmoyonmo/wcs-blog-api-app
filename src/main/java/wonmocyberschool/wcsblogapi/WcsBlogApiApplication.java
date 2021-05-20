ed4455a8e9e4dab071b9e8788f041aecbb103a3f
yonmoyonmo <yonmosound@gmail.com>
2021-05-21 07:16:43 +0900

    project setting

---
+++ src/main/java/wonmocyberschool/wcsblogapi/WcsBlogApiApplication.java
@@ -0,0 +1,13 @@
+package wonmocyberschool.wcsblogapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WcsBlogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WcsBlogApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wonmocyberschool/wcsblogapi/WcsBlogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package wonmocyberschool.wcsblogapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WcsBlogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
